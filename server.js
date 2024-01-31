ae27d1fbfed1468c02d180f355cc2ff7dda55386
Mohamed Saleh <mohamed20163858@gmail.com>
2024-02-01 00:06:34 +0200

    Intialize  backend app

---
+++ server.js
@@ -0,0 +1,15 @@
+// importing
+import express from "express";
+// app config
+const app = express();
+const port = process.env.PORT || 9000;
+// middlewares
+
+// DB config
+
+// ??????
+
+// api routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
+// listener
+app.listen(port, () => console.log(`Listening on port ${port}`));
